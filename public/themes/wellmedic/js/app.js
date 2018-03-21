054f586cb9195761bdcb81d48a9d97dccb2bd05a
nesacar <nebojsart1409@yahoo.com>
2018-03-21 13:58:03 +0100

    first commit

---
+++ public/themes/wellmedic/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ministudio2017 on 3/21/2018.
+ */
--- resources/views/themes/wellmedic/index.blade.php
+++ resources/views/themes/wellmedic/index.blade.php
@@ -1,1 +1,12 @@
-<p>over here</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="{{ url('themes/wellmedic/css/app.css') }}">
+</head>
+<body>
+
+<script src="{{ url('themes/wellmedic/js/app.js') }}"></script>
+</body>
+</html>
