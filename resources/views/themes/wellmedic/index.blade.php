611a20bfabd37ff40942e55c9dda34c81037d2f0
VacaSan <kotaracvladan@gmail.com>
2018-03-22 16:54:36 +0100

    add sticky footer. styled footer copy section

--- resources/views/themes/wellmedic/index.blade.php
+++ resources/views/themes/wellmedic/index.blade.php
@@ -6,13 +6,24 @@
     <link rel="stylesheet" href="{{ url('themes/wellmedic/css/index.css') }}">
 </head>
 <body>
-  @include('themes/wellmedic/partials/header')
+  <div class="content">
+    @include('themes/wellmedic/partials/header')
 
-  <h1 class="headline">Headline</h1>
-  <h2 class="title"><span class="thin">Prefix</span> Title</h2>
-  <p class="caption">Caption</p>
-  <h3 class="subheading">Subheading</h3>
-  <p>This is a block of text.</p>
+    <div class="container">
+      <h1 class="headline">Headline</h1>
+      <h2 class="title"><span class="thin">Prefix</span> Title</h2>
+      <p class="caption">Caption</p>
+      <h3 class="subheading">Subheading</h3>
+      <p>This is a block of text.</p>
+
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Primary Button</button>
+      <button class="btn btn-primary btn-outline">Outline Button</button>
+    </div>
+
+  </div>
+
+  @include('themes/wellmedic/partials/footer')
   
 <script src="{{ url('themes/wellmedic/js/index.js') }}"></script>
 </body>
---
+++ resources/views/themes/wellmedic/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="container footer_copy">
+    <span>Copyright &copy; <span id="jsDate">2018</span> Wellmedic | All rights reserved.</span>
+    <span>Developed by <a href="https://ministudio.rs/" target="_blank" rel="noopener noreferrer">Mini STUDIO Publishing Group</a></span>
+  </div>
+</footer>
