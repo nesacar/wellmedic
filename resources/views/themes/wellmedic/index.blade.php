c651ba1f2718aaaf139f9a94ca73e13c5ba7bf38
VacaSan <kotaracvladan@gmail.com>
2018-03-22 10:58:10 +0100

    more typography styles

--- resources/views/themes/wellmedic/index.blade.php
+++ resources/views/themes/wellmedic/index.blade.php
@@ -10,7 +10,9 @@
 
   <h1 class="headline">Headline</h1>
   <h2 class="title"><span class="thin">Prefix</span> Title</h2>
+  <p class="caption">Caption</p>
   <h3 class="subheading">Subheading</h3>
+  <p>This is a block of text.</p>
   
 <script src="{{ url('themes/wellmedic/js/index.js') }}"></script>
 </body>
