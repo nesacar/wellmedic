c7d0409a3687a3e684d36287467978586da7dff6
VacaSan <kotaracvladan@gmail.com>
2018-03-23 12:49:39 +0100

    add footer structure

--- resources/views/themes/wellmedic/partials/footer.blade.php
+++ resources/views/themes/wellmedic/partials/footer.blade.php
@@ -1,4 +1,65 @@
 <footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-12">
+        <img src="{{ url('themes/wellmedic/images/wellmedic-logo.png') }}" alt="wellmedic logo">
+      </div>
+      <div class="col-lg-5 col-md-6">
+        <h5 class="section-title">Nađite nas</h5>
+        <div class="row">
+          <div class="col-6">
+            <div>
+              <strong>TC City Passage</strong>
+              <div>Obilićev Venac 18-20, lokal 2.12</div>
+            </div>
+            <div>
+              <b>Radno vreme</b>
+              <div>Ponedeljak-Petak: 10:00-20:30</div>
+              <div>Subota: 10:00-16:00</div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div>
+              <strong>TC Futura</strong>
+              <div>Nemanjina 40/ll, lokal 3</div>
+              <div>SER, Belgrade 11000</div>
+            </div>
+            <div>
+              <b>Radno vreme</b>
+              <div>Ponedeljak-Petak: 10:00-17:00</div>
+              <div>Subota: 10:00-16:00</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <h5 class="section-title">Kontakt</h5>
+        <div class="row">
+          <div class="col-6">
+            <div>
+              <b>Mobile</b>
+              <div>+381 60 133 8227</div>
+              <div>+381 61 285 5652</div>
+            </div>
+            <div>
+              <div>+381 60 033 8227</div>
+              <div>+381 11 403 9778</div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div>
+              <b>Fax</b>
+              <div>+381 11 403 9175</div>
+            </div>
+            <div>
+              <b>E-mail</b>
+              <div><a href="mailto:info@wellmedic.rs">info@wellmedic.rs</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="container footer_copy">
     <span>Copyright &copy; <span id="jsDate">2018</span> Wellmedic | All rights reserved.</span>
     <span>Developed by <a href="https://ministudio.rs/" target="_blank" rel="noopener noreferrer">Mini STUDIO Publishing Group</a></span>
