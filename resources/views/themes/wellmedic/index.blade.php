cd5796b0efc2dd78531b75dfe0110336a53c50f2
VacaSan <kotaracvladan@gmail.com>
2018-03-23 12:22:59 +0100

    add meta tags

--- resources/views/themes/wellmedic/index.blade.php
+++ resources/views/themes/wellmedic/index.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Wellmedic - @yield('title')</title>
     <link rel="stylesheet" href="{{ url('themes/wellmedic/css/index.css') }}">
 </head>
