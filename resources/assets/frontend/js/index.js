a1a4492456e58799837b685b3232007a519e64c7
VacaSan <kotaracvladan@gmail.com>
2018-03-30 12:40:34 +0200

    transform nav drower to class component

--- resources/assets/frontend/js/index.js
+++ resources/assets/frontend/js/index.js
@@ -1,10 +1,10 @@
 // window.$ = window.jQuery = require('jquery');
 // require('bootstrap');
 
-import drawer from './components/drawer';
+import Drawer from './components/drawer';
 import LazyImages from './components/lazy-images';
 import Toolbar from './components/toolbar';
 
-const toolbar = new Toolbar();
-drawer.init();
+Toolbar.init();
+Drawer.init();
 LazyImages.init();
