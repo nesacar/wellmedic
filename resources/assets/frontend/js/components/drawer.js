3506d78862c5604084a08b45a6327c0fde39e29e
VacaSan <kotaracvladan@gmail.com>
2018-03-31 14:09:42 +0200

    basic swipe close drawer

--- resources/assets/frontend/js/components/drawer.js
+++ resources/assets/frontend/js/components/drawer.js
@@ -1,5 +1,12 @@
 class Drawer {
-  static get ACTIVE_CLASS () { return 'active'; }
+  static get ACTIVE_CLASS () {
+    return 'active';
+  }
+
+  static get ANIMATABLE_CLASS () {
+    return 'animatable';
+  }
+
   static init () {
     new Drawer();
   }
@@ -9,9 +16,15 @@ class Drawer {
     this._drawerEl = document.getElementById('mobileNav');
     this._drawerContainerEl = this._drawerEl.querySelector('.drawer_drawer');
 
+    this._startX = 0;
+    this._currentX = 0;
+
     this._show = this._show.bind(this);
     this._hide = this._hide.bind(this);
-    this._blockClicks = this._blockClicks.bind(this);
+    this._onTouchStart = this._onTouchStart.bind(this);
+    this._onTouchMove = this._onTouchMove.bind(this);
+    this._onTouchEnd = this._onTouchEnd.bind(this);
+    this._onTransitionEnd = this._onTransitionEnd.bind(this);
 
     this._addEventListeners();
   }
@@ -20,20 +33,57 @@ class Drawer {
     this._showButtonEl.addEventListener('click', this._show);
     this._drawerEl.addEventListener('click', this._hide);
     this._drawerContainerEl.addEventListener('click', this._blockClicks);
+
+    this._drawerEl.addEventListener('touchstart', this._onTouchStart);
+    this._drawerEl.addEventListener('touchmove', this._onTouchMove);
+    this._drawerEl.addEventListener('touchend', this._onTouchEnd);
   }
 
   _blockClicks (evt) {
     evt.stopPropagation();
   }
 
+  _onTouchStart (evt) {
+    this._startX = evt.touches[0].pageX;
+    this._currentX = this._startX;
+  }
+
+  _onTouchMove (evt) {
+    this._currentX = evt.touches[0].pageX;
+
+    const translateX = Math.min(0, this._currentX - this._startX);
+    this._drawerContainerEl.style.transform = `translateX(${translateX}px)`;
+  }
+
+  _onTouchEnd (evt) {
+    const translateX = Math.min(0, this._currentX - this._startX);
+    this._drawerContainerEl.style.transform = 'initial';
+
+    if (translateX < -30) {
+      this._drawerContainerEl.style.transform = '';
+      this._hide();
+    }
+  }
+
+  _onTransitionEnd () {
+    this._drawerEl.classList.remove(Drawer.ANIMATABLE_CLASS);
+    this._drawerEl.removeEventListener('transitionend', this._onTransitionEnd);
+  }
+
   _show () {
+    this._drawerEl.classList.add(Drawer.ANIMATABLE_CLASS);
     this._drawerEl.classList.add(Drawer.ACTIVE_CLASS);
     document.body.classList.add('no-scroll');
+
+    this._drawerEl.addEventListener('transitionend', this._onTransitionEnd);
   }
 
   _hide () {
+    this._drawerEl.classList.add(Drawer.ANIMATABLE_CLASS);
     this._drawerEl.classList.remove(Drawer.ACTIVE_CLASS);
     document.body.classList.remove('no-scroll');
+
+    this._drawerEl.addEventListener('transitionend', this._onTransitionEnd);
   }
 }
 
