10a4b551727152609c1a85c766eb3b2293b12dca
nesacar <nebojsart1409@yahoo.com>
2018-04-12 14:58:42 +0200

    favicon images

--- resources/views/themes/wellmedic/index.blade.php
+++ resources/views/themes/wellmedic/index.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('themes/'.$theme.'/images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('themes/'.$theme.'/images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('themes/'.$theme.'/images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('themes/'.$theme.'/images/favicon/site.webmanifest') }}">
     <link rel="stylesheet" href="{{ url('themes/wellmedic/css/index.css') }}">
     @yield('seo_social_stuff')
     <!-- fonts -->
