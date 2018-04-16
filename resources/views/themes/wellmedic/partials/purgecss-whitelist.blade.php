e03cf627508bf348b2c640c11f44694fed55e75a
VacaSan <kotaracvladan@gmail.com>
2018-04-16 12:10:34 +0200

    swiper pagination missing bugfix

--- resources/views/themes/wellmedic/partials/purgecss-whitelist.blade.php
+++ resources/views/themes/wellmedic/partials/purgecss-whitelist.blade.php
@@ -1,6 +1,6 @@
 <div
   style="display: none;"
-  class="swiper-pagination-bullet swiper-pagination-bullet-active swiper-slide-active"
+  class="swiper-container-horizontal swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-bullet swiper-pagination-bullet-active swiper-slide-active"
 >
   PurgeCSS Whitelist
 </div>
