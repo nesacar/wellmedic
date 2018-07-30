4169b0ec187de4c34bba6a5f8c1f64fabdeef298
VacaSan <kotaracvladan@gmail.com>
2018-07-30 12:31:13 +0200

    Edit about page.

--- resources/views/themes/wellmedic/pages/about.blade.php
+++ resources/views/themes/wellmedic/pages/about.blade.php
@@ -40,7 +40,7 @@
     <div class="hero" id="hero">
       <h1 class="title hero_title">O Nama</h1>
       <figure class="plant plant--spoon">
-        <img src="{{ url('themes/wellmedic/images/demo/spoon.png') }}" alt="drvena kašika">
+        <img src="{{ url('themes/wellmedic/images/plants/spoon.png') }}" alt="drvena kašika">
       </figure>
     </div>
     <div id="breadcrumb">
@@ -59,14 +59,15 @@
     <div class="row about">
       <div class="col-lg-5">
         <div class="image lazy-image"
-          data-src="https://images.pexels.com/photos/259704/pexels-photo-259704.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          data-src="{{ url('themes/wellmedic/images/about/mushrooms.jpg') }}"
         ></div>
       </div>
       <div class="col-lg-7">
         <h1 class="about_logo">
           <img src="{{ url('themes/wellmedic/images/wellmedic-logo.png') }}" alt="wellmedic logo">
         </h1>
-        <p>WELLMEDIC d.o.o. Srbija, sa sedištem u Beogradu ul. Kneza Mihajla br. 17. je jedna od najperspektivnijih veleprodaja medicinske opreme i dijetetskih suplemenata u regionu.<br/>Poznati smo po bogatom asortimanu opreme i proizboda kao i po brzoj i pouzdanoj dostavi.</p>
+        <p>WELLMEDIC d.o.o. je ekskluzivni uvoznik, distributer i zastupnik za Evropu kompanije Nanjing Xiyan Bio-Pharmaceutical Technology Co, Ltd, koja je svetski lider u proizvodnji vrhunskih preparata baziranih na tradicionalnoj kineskoj medicini.</p>
+        <p>Od 2015. godine pouzdanom i brzom uslugom, kao i kvalitetnim prirodnim preparatima od sirovina iz organskog usgoja radimo u službi Vašeg zdravlja, trudeći se da budete ZDRAVI, LEPI I SREĆNI.</p>
         <div class="d-flex align-items-center primary">
           <span class="mr-3">Pratite nas na:</span>
           <ul class="social">
@@ -94,24 +95,6 @@
           </ul>
         </div>
 
-      </div>
-    </div>
-    <div class="row flex-row-reverse about">
-      <div class="col-lg-4">
-        <div class="image image--square fit-height lazy-image"
-          data-src="https://cdn7.bigcommerce.com/s-u0uja8gg4w/images/stencil/500x650/products/155/668/B000178_use__01779.1519163981.png?c=2"
-          data-alt="genoderma lucidium"
-        ></div>
-      </div>
-      <div class="col-lg-8">
-        <p>Wellmedic je ekskluzivni uvoznik i distributer proizvoda Nanjing Xiyan Bio-Pharmaceutical Technology Co, Ltd za Evropu. <br/>Najpoznatiji proizvodi iz naše ponude su:</p>
-        <ul class="list list--bullet">
-          <li class="list--bullet_item">Meke gel kapsule na bazi ulja iz spora pečurke Ganoderma lucidum</li>
-          <li class="list--bullet_item">Kapsule na bazi Ženšena i roga mladog jelena</li>
-          <li class="list--bullet_item">Pasta za zube na bazi Zelenog propolisa i ulja Ganoderme lucidum</li>
-        </ul>
-        <p>Naša kuća WELLMEDIC d.o.o. Srbija u potpunosti posluje u skladu sa standardima ISO 9001:2008</p>
-        <p>Wellmedic Ltd. Srbija je ekskluzivni uvoznik i distributer proizvoda Nanjing Xiyan Bio-Pharmaceutical Technology Co, Ltd proizvoda za Evropu.</p>
       </div>
     </div>
   </div>
